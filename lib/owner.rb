445fe1b1fdde22791c4b332c37e3fa8e42dd0dd0
Andrew Waller <andrewhwaller@gmail.com>
2019-03-22 04:15:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,15 +43,15 @@ class Owner
   end
 
   def walk_dogs
-    dogs.mood = "happy"
+    self.dogs.mood = "happy"
   end
 
   def play_with_cats
-    cats.mood = "happy"
+    self.cats.mood = "happy"
   end
 
   def feed_fish
-    fishes.mood = "happy"
+    self.fishes.mood = "happy"
   end
 
   def sell_pets
